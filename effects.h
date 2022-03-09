e16d77ba0a79c4377875553ce17b19005204fd74
ронин <49647614+ronin49@users.noreply.github.com>
2022-03-09 16:30:40 +0000

    Add files via upload

--- effects.h
+++ effects.h
@@ -1,41 +1,17 @@
-/*
-sfml example (if you don't want sfml read further)
-
-void f(sf::Time dt){
-static sf::Time allTime;
+#define EFFECT(function,duration) if(elapsed < duration) {DURATION=duration;function return true;} else elapsed -= duration;
+#define PROGRESS() (elapsed/DURATION)
+template <typename T>
+struct Effect{
+T allTime{};
+T elapsed{};
+T DURATION{};
+void upd(T dt){
 allTime += dt;
-sf::Time elapsed = allTime;
-EFFECT(sf::seconds(5))
-{
-std::cout << "first five seconds";
+elapsed = allTime;
+if(!continueEffects()){
+allTime = T();
+continueEffects();
 }
-EFFECT(sf::milliseconds(500))
-std::cout << "then for a half of second";
 }
-
-IF YOU DON'T WANT SFML?
-just redefine EFFECT_TIME_ZERO and pass your preffered type to EFFECT macro)
-
-non-sfml example using int as number of milliseconds
-
-#define EFFECT_TIME_ZERO 0
-#include "effects.h"
-void f(int dt){
-static int allTime;
-allTime += dt;
-int elapsed = allTime;
-EFFECT(5)
-{
-std::cout << "first five milliseconds";
-}
-EFFECT(500)
-std::cout << "then five hundred milliseconds";
-}
-
-*/
-
-#ifndef EFFECT_TIME_ZERO
-#include <SFML/Graphics.hpp>
-#define EFFECT_TIME_ZERO sf::Time::Zero
-#endif
-#define EFFECT(duration) if(elapsed < EFFECT_TIME_ZERO) return; elapsed -= duration; if(elapsed < EFFECT_TIME_ZERO) 
+virtual bool continueEffects() = 0;
+};
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "effects.h"
+#include <iostream>
+#include <SFML/Graphics.hpp>
+struct myEffect : Effect<sf::Time> {
+bool continueEffects(){
+ EFFECT({std::cout << "1: " << PROGRESS() << std::endl;},sf::seconds(100))
+ EFFECT({std::cout << "2: " << PROGRESS() << std::endl;},sf::seconds(100))
+}
+} e;
+main()
+{
+e.upd(sf::seconds(50));
+e.upd(sf::seconds(100));
+}
+
